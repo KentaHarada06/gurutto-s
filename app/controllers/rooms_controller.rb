4b9e2c0e34b4bcefe08509e6a26f4679be1dcbf5
KentaHarada06 <haradakenta4622@gmail.com>
2020-11-28 14:15:15 +0900

    ルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,24 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render new_room_path
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:room_name, :room_description, user_ids: []).merge(owner_id: current_user.id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users, dependent: :destroy
+
+  with_options presence: true do
+    validates :room_name
+    validates :room_description
+    validates :owner_id
+  end
 end
