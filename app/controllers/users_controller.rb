418aad4e5cc1e496ec7bd35a199f9fca2888f9ff
KentaHarada06 <haradakenta4622@gmail.com>
2020-12-25 14:31:22 +0900

    ユーザー詳細表示の機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @favorites = Favorite.where(user_id: current_user.id).order(created_at: 'DESC')
+    @favorite_posts = Favorite.favorite_list(@favorites)
   end
 
   def edit
